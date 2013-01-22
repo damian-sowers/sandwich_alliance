a21b27b942a4b5f8bd9b513bc6918e54aaa9265a
Damian Sowers <damian.sowers@gmail.com>
2013-01-22 14:39:31 -0800

    fixing errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SandwichAlliance::Application.routes.draw do
   # first created -> highest priority.
 
   root :to => 'home#index'
-  match "/home" => 'home#index'
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
