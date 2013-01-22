69cf26266a1310e5c48cf1a7df767d4a60a3edfc
Damian Sowers <damian.sowers@gmail.com>
2013-01-22 14:22:03 -0800

    compiled assets;

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,9 @@ module SandwichAlliance
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    #to satisfy heroku
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
