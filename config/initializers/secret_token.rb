ee4fc93927fd5c086d6b83d5a6ccc81fbf8f5d2d
Damian Sowers <damian.sowers@gmail.com>
2013-01-22 13:13:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandwichAlliance::Application.config.secret_token = '1168b00c394de80ca46361d2b7b4f69d0fc28a19d30c1c4145131a33c95df469f82ef96c3bd0b91af83c4fee5ac4ae76e4f807e8310119cba39155be2e388e8e'
